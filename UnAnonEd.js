7c3dc0c3b54186ba86b0833fa71f674b19590185
coldtaco <36543008+coldtaco@users.noreply.github.com>
2023-04-04 16:27:31 +1000

    Update UnAnonEd.js

--- UnAnonEd.js
+++ UnAnonEd.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://edstem.org/au/courses/10360/*
+// @match        https://edstem.org/au/courses/*/discussion/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=edstem.org
 // @grant        none
 // ==/UserScript==
@@ -19,16 +19,15 @@
     }
 
     async function requestJSON(url){
-        // var request = new Request(url, options);
-        /*fetch(request).then((response) => response.json()).then((res) => {
-            return res;
-        });*/
+        /*
+        Resolves JSON request
+        */
         const req = await fetch(url, options);
         return req.json();
     };
 
-    async function getAuthorDetails(author){
-        var url = `https://edstem.org/api/courses/10360/users/${author}?emails=true`
+    async function getAuthorDetails(author, course_id){
+        var url = `https://edstem.org/api/courses/${course_id}/users/${author}?emails=true`
         return requestJSON(url).then((res) => {
             return res.user.name;
         })
@@ -37,24 +36,33 @@
     async function getPostAuthor(post){
         var url = `https://edstem.org/api/threads/${post}`
         return requestJSON(url).then((res) => {
-            return res.thread.user_id;
+            return {'user_id': res.thread.user_id,
+                    'course_id': res.thread.course_id};
         })
     };
 
     async function fixAnon(ele){
+        /*
+        Grabs thread id to figure out author id, uses author id to grab name, then inserts name in left sidebar
+        */
         var href = ele.getElementsByTagName('a')[0].href;
         var name_element = ele.getElementsByClassName('dft-user')[0];
 
         if (name_element.innerHTML.localeCompare('Anonymous') != 0) {return;}
 
         var post_id = href.split('/').pop();
-        var author_id = await getPostAuthor(post_id)
-        var author_name = await getAuthorDetails(author_id)
+        var res = await getPostAuthor(post_id)
+        var user_id = res.user_id;
+        var course_id = res.course_id
+        var author_name = await getAuthorDetails(user_id, course_id)
 
         name_element.innerHTML = `${author_name} (Anonymous)`;
     };
 
     async function fixWrapper(event){
+        /*
+        Wrapper to get element from event and pass it to fixAnon
+        */
         var ele = event.target;
         if (ele.nodeName.localeCompare('#text') == 0){return;}
         if (ele.className.localeCompare('dlv-item') != 0){return;}
@@ -83,18 +91,21 @@
     }
 
     function fixExisting(elements){
+        /*
+        Iterate through list of elements already loaded
+        */
         for (let i = 0; i < elements.length; i++){
             fixAnon(elements[i]);
         }
     };
 
+    // Wait for element
     waitForElm('.dtl-group-heading').then((elm) => {
         console.log('Element is ready');
         console.log(elm.textContent);
-        const ele = document.getElementById('x7-option-1283739');
+        // Grab left sidebar, fix existing and add eventlistener
         const discussion_list = document.getElementsByClassName('discuss-thread-list')[0];
         discussion_list.addEventListener("DOMNodeInserted", fixWrapper);
         fixExisting(discussion_list.getElementsByClassName('dlv-item'));
     });
-
 })();
